53947ce54e270a6fae56476d267437d26b74862c
HungKuei <hungkuei@163.com>
2018-11-09 17:13:05 +0800

    commit code

--- smartcampus-admin/src/main/java/com/smartcampus/controller/SampleController.java
+++ smartcampus-admin/src/main/java/com/smartcampus/controller/SampleController.java
@@ -4,15 +4,16 @@ package com.smartcampus.controller;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @EnableAutoConfiguration
 public class SampleController {
  
-	@RequestMapping("/")
+	@RequestMapping(value = "/index")
 	@ResponseBody
 	public String home() {
-		return "Hello Spring Boot!";
+		return "index";
 	}
 }
